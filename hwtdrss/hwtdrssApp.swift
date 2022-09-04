410116034033dc959e4aac1bcf1eccdd2e5347ae
Count174 <icejayms@gmail.com>
2022-09-04 14:16:02 +0500

    Initial Commit

---
+++ hwtdrss/hwtdrssApp.swift
@@ -0,0 +1,17 @@
+//
+//  hwtdrssApp.swift
+//  hwtdrss
+//
+//  Created by Кирилл Кочетков on 04.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct hwtdrssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
